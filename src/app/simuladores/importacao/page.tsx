80e727417940d518123d4bd6c8db30695cddaecb
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2025-06-21 01:53:22 -0300

    fix(build): mark import simulator page as client component

--- src/app/simuladores/importacao/page.tsx
+++ src/app/simuladores/importacao/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import MainLayout from '../../../components/layout/MainLayout';
 import ImportCostCalculator from '../../../components/simulators/ImportCostCalculator';
