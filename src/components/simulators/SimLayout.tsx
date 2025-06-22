4248a84276206caaa9545ae0b5beef2429cc4d7b
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2025-06-22 15:14:29 -0300

    style(layout): increase top padding to 1cm and remove extra margin under ticker

--- src/components/simulators/SimLayout.tsx
+++ src/components/simulators/SimLayout.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 export default function SimLayout({ quotes, calculator, guide }: Props) {
   return (
-    <div className="container mx-auto max-w-7xl space-y-10 pt-6 md:pt-8">
-      <div className="grid gap-8 lg:grid-cols-[10cm_10cm_15cm] mt-[0.5cm]">
+    <div className="container mx-auto max-w-7xl space-y-10 pt-[1cm]">
+      <div className="grid gap-8 lg:grid-cols-[10cm_10cm_15cm]">
         <aside>{quotes}</aside>
         <section className="col-span-2 lg:col-start-2 lg:col-span-2">{calculator}</section>
       </div>
