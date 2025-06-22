e22fe998e1b56dc18f803c06d9d2645ae70cb1ac
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2025-06-22 15:23:05 -0300

    style(simlayout): increase top padding to 1.5cm to avoid header overlap

--- src/components/simulators/SimLayout.tsx
+++ src/components/simulators/SimLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function SimLayout({ quotes, calculator, guide }: Props) {
   return (
-    <div className="container mx-auto max-w-7xl space-y-10 pt-[1cm]">
+    <div className="container mx-auto max-w-7xl space-y-10 pt-[1.5cm]">
       <div className="grid gap-8 lg:grid-cols-[10cm_10cm_15cm]">
         <aside>{quotes}</aside>
         <section className="col-span-2 lg:col-start-2 lg:col-span-2">{calculator}</section>
